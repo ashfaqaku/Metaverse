a5eec0a8787b0b23455f75395142c82b46891d9b
Muhammad Ashfaq <ashfaqaku@gmail.com>
2022-10-01 19:07:53 +0500

    Update Index.js

--- Quatar1/Assigments/Q2/Index.js
+++ Quatar1/Assigments/Q2/Index.js
@@ -1,18 +1,15 @@
 
-var personName="Hello Eric\n";
-var son = "kaloO heloH";
+var personName="hello eric\n";
 
 document.write(personName.toLowerCase());
 
 document.write(personName.toUpperCase());
-function titleCase(son) {
-    var sentence = son.toLowerCase().split(" ");
+function titleCase(string) {
+    var sentence = string.toLowerCase().split(" ");
     for(var i = 0; i< sentence.length; i++){
        sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
     }
  document.write(sentence.join(" "));
- console.log(sentence);
  return sentence;
  }
-//  personName.titleCase();
-console.log(son);
+ titleCase(personName);
